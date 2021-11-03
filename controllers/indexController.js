d249060bc9f69f4cdba4d1702fdbc8613210cbc1
olivera rocio <layoyi2015@gmail.com>
2021-11-03 18:30:51 -0300

    dinamiza about

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const menu = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','menu.json'),'utf-8'));
 const platoTipico = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','platoTipico.json'),'utf-8'));
+const about = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','about.json'),'utf-8'));
 
 
 module.exports = {
     index : (req, res) => res.render("index",{
-        menu
+        menu,
+        about
     }),
     
     detail : (req, res) =>{
